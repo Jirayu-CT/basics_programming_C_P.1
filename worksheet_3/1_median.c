02e02f5b53ad9cc05faec05e4041101bfd29c69e
Jirayu-CT <125994181+Jirayu-CT@users.noreply.github.com>
2023-07-12 20:11:43 +0700

    Create 1_median.c

---
+++ worksheet_3/1_median.c
@@ -0,0 +1,51 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    int a, b, c, median;
+    
+    
+    printf("Press Input Number1: ");
+    scanf("%d", &a);
+    
+    printf("Press Input Number2: ");
+    scanf("%d", &b);
+    
+    printf("Press Input Number3: ");
+    scanf("%d", &c);
+    
+    //ascending
+    if(a>b){
+        int tamp = a;
+        a = b;
+        b = tamp;
+    }
+    
+    if(b>c){
+        int tamp = b;
+        b = c;
+        c = tamp;
+    }
+    
+    if(a>b){
+        int tamp = a;
+        a = b;
+        b = tamp;
+    }
+    
+    //median
+    if(a<=b && b<=c){
+        median = b;
+    }
+    else if(b<=a && a<=c){
+        median = a;
+    }
+    else{
+        median = c;
+    }
+    
+    printf("%d", median);
+    
+    return 0;
+}
